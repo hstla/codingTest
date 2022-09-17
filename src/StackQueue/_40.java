88285e3a3af0dd210489dc30fad1ed05f32500d3
hstla <ree7243@gmail.com>
2022-09-17 22:33:29 +0900

    쇠막대기 문제

---
+++ src/StackQueue/_40.java
@@ -0,0 +1,16 @@
+package StackQueue;
+
+import java.util.Scanner;
+
+public class _40 {
+  public int solution(String str){
+    int answer=0;
+    return answer;
+  }
+  public static void main(String[] args) {
+    _40 T=new _40();
+    Scanner scanner=new Scanner(System.in);
+    String str=scanner.nextLine();
+    System.out.println(T.solution(str));
+  }
+}

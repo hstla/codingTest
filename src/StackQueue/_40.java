accda2cc492fc4a82968aef9d2e28a04199bdcab
hstla <ree7243@gmail.com>
2022-09-19 09:31:39 +0900

    쇠막대기 문제

--- src/StackQueue/_40.java
+++ src/StackQueue/_40.java
@@ -1,10 +1,21 @@
 package StackQueue;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class _40 {
   public int solution(String str){
     int answer=0;
+    Stack<Character> stack=new Stack<>();
+    for(int i=0;i<str.length();i++){
+      if(str.charAt(i)=='(') stack.push(str.charAt(i));
+      else{
+        stack.pop();
+        if(str.charAt(i-1)=='('){
+          answer+=stack.size();
+        }else answer++;
+      }
+    }
     return answer;
   }
   public static void main(String[] args) {

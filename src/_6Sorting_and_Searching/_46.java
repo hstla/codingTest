7fc578826b7b9d755921da95d93689d22f688b9c
hstla <ree7243@gmail.com>
2022-10-03 18:33:18 +0900

    선택정렬 문제
    선택정렬을 이용한 오름차순

--- src/_6Sorting_and_Searching/_46.java
+++ src/_6Sorting_and_Searching/_46.java
@@ -1,22 +1,23 @@
 package _6Sorting_and_Searching;
 
+
 import java.util.Scanner;
 
-public class _45 {
+public class _46 {
   public int[] solution (int n, int[] arr) {
-    for (int i = 0;i < n - 1; i++) {
-      for (int j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j+1]) {
-          int tmp = arr[j];
-          arr[j] = arr[j+1];
-          arr[j+1] = tmp;
-        }
+    for (int i = 1; i < n;i++) {
+      int tmp = arr[i];
+      for (int j = i - 1; j >= 0; j--) {
+        if (arr[j] > tmp) {
+          arr[j + 1] = arr[j];
+        } else break;
+        arr[j] = tmp;
       }
     }
     return arr;
   }
   public static void main(String[] args) {
-    _45 T = new _45();
+    _46 T = new _46();
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int[] arr = new int[n];

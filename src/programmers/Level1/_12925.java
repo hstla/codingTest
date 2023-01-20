6fcf04f80c9d30da7033de9edf1dd6fa8077f309
hstla <ree7243@gmail.com>
2023-01-20 23:31:17 +0900

    programmers level1 12925 풀기

---
+++ src/programmers/Level1/_12925.java
@@ -0,0 +1,7 @@
+package programmers.Level1;
+
+public class _12925 {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
---
+++ src/programmers/Level1/_133502.java
@@ -0,0 +1,8 @@
+package programmers.Level1;
+
+public class _133502 {
+  public int solution(int[] ingredient) {
+    int answer = 0;
+    return answer;
+  }
+}

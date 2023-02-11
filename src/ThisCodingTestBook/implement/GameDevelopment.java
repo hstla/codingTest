5dc4878311c838d7943608457a6d42e1652885ea
seong <ree7243@gmail.com>
2023-02-11 15:26:11 +0900

    implement update

--- src/ThisCodingTestBook/implement/GameDevelopment.java
+++ src/ThisCodingTestBook/implement/GameDevelopment.java
@@ -1,37 +1,51 @@
 package ThisCodingTestBook.implement;
 
 public class GameDevelopment {
-    public static int[][] d = new int[50][50];
 
-    public static int turn_left(int direction) {
+    public static int n, m, x, y, direction;
+    // 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+    public static int[][] d = new int[50][50];
+    // 전체 맵 정보
+//    public static int[][] arr = new int [50][50];
+
+    // 북, 동, 남, 서 방향 정의
+    public static int dx[] = {-1, 0, 1, 0};
+    public static int dy[] = {0, 1, 0, -1};
+
+    // 왼쪽으로 회전
+    public static void turn_left() {
         direction -= 1;
         if (direction == -1) direction = 3;
-        return direction;
     }
 
-    public int solution(int n, int m, int x, int y, int direction, int[][] arr) {
-        int answer = 1;
-        int turn_time = 0;
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
+    public static void main(String[] args) {
+        GameDevelopment T = new GameDevelopment();
+        n = 4; m = 4; x = 1; y = 1; direction = 0;
         d[x][y] = 1;
+        int[][] arr = {{1, 1, 1, 1}, {1, 0, 0, 1}, {1, 1, 0, 1}, {1, 1, 1, 1}};
 
+        int cnt = 1;
+        int turn_time = 0;
         while (true) {
-            turn_left(direction);
+            turn_left();
             int nx = x + dx[direction];
-            int ny = x + dy[direction];
+            int ny = y + dy[direction];
 
             if (d[nx][ny] == 0 && arr[nx][ny] == 0) {
                 d[nx][ny] = 1;
                 x = nx;
                 y = ny;
-                answer += 1;
+                cnt += 1;
                 turn_time = 0;
                 continue;
-            } else turn_time += 1;
+            }
+
+            else turn_time += 1;
+
             if (turn_time == 4) {
                 nx = x - dx[direction];
                 ny = y - dy[direction];
+
                 if (arr[nx][ny] == 0) {
                     x = nx;
                     y = ny;
@@ -40,13 +54,6 @@ public class GameDevelopment {
                 turn_time = 0;
             }
         }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        GameDevelopment T = new GameDevelopment();
-        int n = 4, m = 4, a = 1, b = 1, d = 0;
-        int[][] arr = {{1, 1, 1, 1}, {1, 0, 0, 1}, {1, 1, 0, 1}, {1, 1, 1, 1}};
-        T.solution(n, m, a, b, d, arr);
+        System.out.println(cnt);
     }
 }

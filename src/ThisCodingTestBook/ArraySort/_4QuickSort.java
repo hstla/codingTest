1ffc225def1f1540566353ea7589a3e4c97b32dd
seong <ree7243@gmail.com>
2023-02-16 15:16:18 +0900

    sort updates

---
+++ src/ThisCodingTestBook/ArraySort/_4QuickSort.java
@@ -0,0 +1,37 @@
+package ThisCodingTestBook.ArraySort;
+
+public class _4QuickSort {
+
+    public static void quickSort(int[] arr, int start, int end) {
+//        int[] arr = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
+        if (start >= end) return;
+        int pivot = start;
+        int left = start + 1;
+        int right = end;
+        while (left <= right) {
+            while (left <= end && arr[left] <= arr[pivot]) left++;
+            while (right > start && arr[right] >= arr[pivot]) right--;
+            if (left > right) {
+                int temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        quickSort(arr, start, right - 1);
+        quickSort(arr, right + 1, end);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[] arr = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        quickSort(arr, 0, n - 1);
+
+        for (int i : arr) System.out.printf("i = " + i + "\t");
+
+    }
+}
--- src/ThisCodingTestBook/DFS_BFS/_11EscapeFromTheMaze.java
+++ src/ThisCodingTestBook/DFS_BFS/_11EscapeFromTheMaze.java
@@ -1,8 +1,9 @@
 package ThisCodingTestBook.DFS_BFS;
+
 import java.util.*;
 
 
-class Node1{
+class Node1 {
     private int x;
     private int y;
 
@@ -14,26 +15,27 @@ class Node1{
     public int getX() {
         return this.x;
     }
+
     public int getY() {
         return this.y;
     }
 }
 
 public class _11EscapeFromTheMaze {
-    public static int n,m;
+    public static int n = 5, m = 6;
     public static int[][] graph = {
-            {1,0,1,0,1,0},
-            {1,1,1,1,1,1},
-            {0,0,0,0,0,1},
-            {1,1,1,1,1,1},
-            {1,1,1,1,1,1}
+            {1, 0, 1, 0, 1, 0},
+            {1, 1, 1, 1, 1, 1},
+            {0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1}
     };
 
 
     public static int dx[] = {-1, 1, 0, 0};
     public static int dy[] = {0, 0, -1, 1};
 
-    public static int bfs (int x, int y) {
+    public static int bfs(int x, int y) {
         Queue<Node1> q = new LinkedList<>();
         q.offer(new Node1(x, y));
 
@@ -42,23 +44,21 @@ public class _11EscapeFromTheMaze {
             x = node.getX();
             y = node.getY();
 
-            for (int i =0;i<4;i++) {
+            for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (graph[nx][ny]==0) continue;
-                if (graph[nx][ny]== 1) {
+                if (graph[nx][ny] == 0) continue;
+                if (graph[nx][ny] == 1) {
                     graph[nx][ny] = graph[x][y] + 1;
                     q.offer(new Node1(nx, ny));
                 }
             }
         }
-        return graph[n-1][m-1];
+        return graph[n - 1][m - 1];
     }
-    public static void main(String[] args) {
-        n = 5;
-        m = 6;
 
-        System.out.println(bfs(0,0));
+    public static void main(String[] args) {
+        System.out.println(bfs(0, 0));
     }
 }

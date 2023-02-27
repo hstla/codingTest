0a0d299d73a839a32755aba210cb5d82aa9c428b
seong <ree7243@gmail.com>
2023-02-27 23:23:44 +0900

    이진 탐색 예시

---
+++ src/ThisCodingTestBook/_7BinarySearch/_5FindParts1.java
@@ -0,0 +1,35 @@
+package ThisCodingTestBook._7BinarySearch;
+
+import java.util.Arrays;
+
+public class _5FindParts1 {
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // 찾은 경우 중간점 인덱스 반환
+            if (arr[mid] == target) return mid;
+                // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+            else if (arr[mid] > target) end = mid - 1;
+                // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+            else start = mid + 1;
+        }
+        return -1;
+    }
+    public static String[] solution(int n, int[] arr1, int m, int[] arr2) {
+        String[] answer = new String[m];
+
+
+
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 3;
+        int[] arr1 = {8,3,7,9,2};
+        int[] arr2 = {5,7,9};
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (String s: solution(n,arr1,m,arr2)) System.out.println(s);
+    }
+}

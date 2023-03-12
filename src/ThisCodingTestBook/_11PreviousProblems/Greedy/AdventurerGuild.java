6daed6c3341bb5af930a23b7dda6e16b5e723f4a
seong <ree7243@gmail.com>
2023-03-12 22:30:44 +0900

    코딩테스트 연습문제

--- src/ThisCodingTestBook/_11PreviousProblems/Greedy/AdventurerGuild.java
+++ src/ThisCodingTestBook/_11PreviousProblems/Greedy/AdventurerGuild.java
@@ -1,7 +1,6 @@
 package ThisCodingTestBook._11PreviousProblems.Greedy;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 /*
@@ -11,14 +10,22 @@ input
 2 3 1 2 2
  */
 public class AdventurerGuild {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        Integer[] arr = new Integer[x];
+        int answer = 0;
+        int count = 0;
+        int[] arr = new int[x];
         for (int i = 0; i < x; i++) arr[i] = sc.nextInt();
-        Arrays.sort(arr,Comparator.reverseOrder());
+        Arrays.sort(arr);
 
-
+        for (int i = 0; i < x; i++) {
+            count += 1;
+            if (count == arr[i]) {
+                count = 0;
+                answer += 1;
+            }
+        }
+        System.out.println(answer);
     }
 }

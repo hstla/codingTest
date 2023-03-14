2d2f1ab8c7c1577db0821d8ffd68c73c98925fd0
seong <ree7243@gmail.com>
2023-03-15 01:44:12 +0900

    코딩테스트 연습문제

--- src/ThisCodingTestBook/_11PreviousProblems/_31GoldMine.java
+++ src/ThisCodingTestBook/_11PreviousProblems/_31GoldMine.java
@@ -1,4 +1,7 @@
 package ThisCodingTestBook._11PreviousProblems;
+
+import java.util.Scanner;
+
 /*
 다이나믹-금광
 input
@@ -9,8 +12,46 @@ input
 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
  */
 public class _31GoldMine {
+    static int testCase, n, m;
+    static int[][] arr = new int[20][20];
+    static int[][] dp = new int[20][20];
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        testCase = sc.nextInt();
+        for (int tc = 0; tc < testCase; tc++) {
+            n = sc.nextInt();
+            m = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = arr[i][j];
+            }
+        }
+        for (int j = 1; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                int leftUp, leftDown, left;
+                // 왼쪽 위
+                if (i == 0) leftUp = 0;
+                else leftUp = dp[i - 1][j - 1];
+                // 왼쪽 아래
+                if (i == n - 1) leftDown = 0;
+                else leftDown = dp[i + 1][j - 1];
+                // 왼쪽
+                left = dp[i][j - 1];
+                dp[i][j] = dp[i][j] + Math.max(leftUp, Math.max(leftDown, left));
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[i][m - 1]);
+        }
+        System.out.println(result);
 
     }
 }

e2f1941bac403d621e817c910cdda75a0d096fd7
seong <ree7243@gmail.com>
2023-04-03 23:28:44 +0900

    튜플

---
+++ src/programmers/Level2/_64065.java
@@ -0,0 +1,12 @@
+package programmers.Level2;
+
+public class _64065 {
+    static public int[] solution(String s) {
+        int[] answer = {};
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
+    }
+}

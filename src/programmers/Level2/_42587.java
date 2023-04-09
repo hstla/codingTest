f0f9b7691eaac800eaaec11e4cfbdce498a9482c
seong <ree7243@gmail.com>
2023-04-09 22:21:27 +0900

    프린터

---
+++ src/programmers/Level2/_42587.java
@@ -0,0 +1,13 @@
+package programmers.Level2;
+
+public class _42587 {
+    static public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 9, 1, 1, 1};
+        System.out.println(solution(arr, 0));
+    }
+}

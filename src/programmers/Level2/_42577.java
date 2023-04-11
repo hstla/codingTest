6b28761e3379af9f7f8e0a003bd6a4be2aacffe4
seong <ree7243@gmail.com>
2023-04-11 23:18:00 +0900

    전화번호 목록

---
+++ src/programmers/Level2/_42577.java
@@ -0,0 +1,19 @@
+package programmers.Level2;
+
+import java.util.Arrays;
+// 전화번호 목록
+public class _42577 {
+    static public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) return false;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"123", "2123", "789"};
+        System.out.println(solution(arr));
+    }
+}
---
+++ src/programmers/Level2/_43165.java
@@ -0,0 +1,26 @@
+package programmers.Level2;
+
+// DFS 재귀함수를 사용
+public class _43165 {
+    static int answer = 0;
+
+    static public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+
+    static void dfs(int n, int sum, int[] numbers, int target) {
+        if (n == numbers.length) {
+            if (sum == target) answer++;
+            return;
+        } else {
+            dfs(n + 1, sum + numbers[n], numbers, target);
+            dfs(n + 1, sum - numbers[n], numbers, target);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1};
+        System.out.println(solution(arr, 4));
+    }
+}

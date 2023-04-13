1b6f049fc47c29bc5db74f4fd07f20a37d9833d0
seong <ree7243@gmail.com>
2023-04-13 23:30:35 +0900

    피로도 순열 알고리즘

---
+++ src/programmers/Level2/_87946.java
@@ -0,0 +1,24 @@
+package programmers.Level2;
+// 피로도
+// 순열 알고리즘
+public class _87946 {
+    static public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {80, 20},
+                {50, 40},
+                {30, 10}
+        };
+        System.out.println(solution(80, arr));
+
+
+        int a = 5;
+        int b = 4;
+        int c = (a > b) ? a : b;
+    }
+}

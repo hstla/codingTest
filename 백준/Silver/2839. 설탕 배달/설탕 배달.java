dbcc408d7b103e58086a512969de7e3c4faa60fa
HWANG Hyun-seong <83001865+hstla@users.noreply.github.com>
2023-05-29 18:33:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int sugar = sc.nextInt();
+        int count = 0;
+        while (true) {
+            if (sugar % 5 == 0) {
+                count += sugar / 5;
+                break;
+            }else {
+                sugar -= 3;
+                count++;
+            }
+            if (sugar < 0) {
+                count = -1;
+                break;
+            } else if (sugar == 0){
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}

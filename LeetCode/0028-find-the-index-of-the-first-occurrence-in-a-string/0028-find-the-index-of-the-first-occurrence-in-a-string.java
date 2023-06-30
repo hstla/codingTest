0c4c11ceb53cda0d5389fddc3f63bd35fcb04f5c
HWANG Hyun-seong <83001865+hstla@users.noreply.github.com>
2023-06-30 17:10:54 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (52.94%) - LeetHub

--- LeetCode/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ LeetCode/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,10 +2,10 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int answer = 0;
         if (haystack.contains(needle)) {
-            answer = haystack.indexOf(needle);            
+            return haystack.indexOf(needle);            
         } else {
             return -1;
         }
-        return answer;
+        // return answer;
     }
 }

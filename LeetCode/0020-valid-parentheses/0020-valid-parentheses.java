d157b9104f3609ba6bff1dc8163df29f32a1ce1f
HWANG Hyun-seong <83001865+hstla@users.noreply.github.com>
2023-06-30 16:55:11 +0900

    Time: 1 ms (98.85%), Space: 40.9 MB (26.53%) - LeetHub

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,16 +5,18 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
                 stack.push(arr[i]);
+            } else if (stack.size() == 0) {
+                return false;
             } else if (arr[i] == ')') {
-                if (stack.size() == 0 || stack.pop() != '(' ) {
+                if (stack.pop() != '(' ) {
                     return false;
                 }
             } else if (arr[i] == ']') {
-                if (stack.size() == 0 || stack.pop() != '[') {
+                if (stack.pop() != '[') {
                     return false;
                 }
             } else if (arr[i] == '}') {
-                if (stack.size() == 0 || stack.pop() != '{') {
+                if (stack.pop() != '{') {
                     return false;
                 }
             }

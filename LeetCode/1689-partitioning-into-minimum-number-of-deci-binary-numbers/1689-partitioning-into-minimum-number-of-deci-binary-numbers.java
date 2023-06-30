943266ae7a4e573b87763dd91928e504e0ad11ee
HWANG Hyun-seong <83001865+hstla@users.noreply.github.com>
2023-06-30 17:23:03 +0900

    Time: 16 ms (24.17%), Space: 44.5 MB (22.57%) - LeetHub

--- LeetCode/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ LeetCode/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,8 +1,9 @@
 class Solution {
     public int minPartitions(String n) {
         int max = Integer.MIN_VALUE;
-        for (char c : n.toCharArray()) {
-            max = (c-'0' > max) ? c-'0':max;
+        for (int c : n.toCharArray()) {
+            int tmp = Character.getNumericValue(c);
+            max = (tmp > max) ? tmp : max;
         }
         return max;
     }
